52a0769d220ad960c7cbf294073496ce2f2efbb0
akirakaito-vietnam <akirakaito.vietnam@proton.me>
2026-02-28 19:17:50 +0700

    .

--- resources/martor-mathjax.js
+++ resources/martor-mathjax.js
@@ -9,24 +9,49 @@ jQuery(function ($) {
                     console.error('MathJax error:', err);
                 });
             } else {
-                console.warn('MathJax not loaded yet');
+                // console.warn('MathJax not loaded yet');
             }
         }
 
         var $jax = $content.find('.require-mathjax-support');
         if ($jax.length) {
             if (!('MathJax' in window)) {
-                var checkMathJax = setInterval(function() {
-                    if (window.MathJax && typeof MathJax.typesetPromise === 'function') {
-                        clearInterval(checkMathJax);
-                        update_math();
+                $.ajax({
+                    type: 'GET',
+                    url: $jax.attr('data-config'),
+                    dataType: 'script',
+                    cache: true,
+                    success: function () {
+                        window.MathJax = window.MathJax || {};
+                        window.MathJax.startup = {typeset: false};
+                        
+                        $.ajax({
+                            type: 'GET',
+                            url: '/static/vnoj/mathjax/3.2.0/es5/tex-chtml.min.js',
+                            dataType: 'script',
+                            cache: true,
+                            success: function() {
+                                var checkMathJax = setInterval(function() {
+                                    if (window.MathJax && typeof MathJax.typesetPromise === 'function') {
+                                        clearInterval(checkMathJax);
+                                        update_math();
+                                    }
+                                }, 100);
+                                
+                                setTimeout(function() {
+                                    clearInterval(checkMathJax);
+                                    console.error('MathJax failed to initialize');
+                                }, 5000);
+                            },
+                            error: function() {
+                                console.error('Failed to load MathJax core');
+                            }
+                        });
+                    },
+                    error: function() {
+                        console.error('Failed to load MathJax config');
                     }
-                }, 100);
-                
-                setTimeout(function() {
-                    clearInterval(checkMathJax);
-                    console.error('MathJax failed to load');
-                }, 5000);
+                });
             } else {
                 update_math();
             }
